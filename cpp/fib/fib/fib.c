321aae9b21c8414e0ed11eafff338c152cc50500
nevergiveupyang1989 <nevergiveupyang1989@gmail.com>
2017-05-10 00:09:32 +0800

    修改fib的算法

--- cpp/fib/fib/fib.c
+++ cpp/fib/fib/fib.c
@@ -22,17 +22,3 @@ int fib(int n){ //µÝ¹é
 	return fib(n - 1) + fib(n - 2);
 }
 
-int fast_fib(int n){//µü´ú
-	int f, g;
-	int i;
-	f = 0;
-	g = 1;
-
-	for (i = 1; i < n; i++){
-		g = f + g;
-		f = g - f;
-	}
-
-	return g;
-	
-}
