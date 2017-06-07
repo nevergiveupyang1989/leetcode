47f7ffb008d8019af0bd7ab8cd8017c8acaa6bbb
nevergiveupyang1989@gmail.com <nevergiveupyang1989@gmail.com>
2017-06-07 23:27:43 +0800

    修改两个数其反转后，降序排列的问题

--- c/searchInRotatedSortedArray/searchInRotatedSortedArray/Source.c
+++ c/searchInRotatedSortedArray/searchInRotatedSortedArray/Source.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 int binarySearch(int* nums, int left, int right, int target)
 {
 	int begin;
@@ -7,12 +7,22 @@ int binarySearch(int* nums, int left, int right, int target)
 	int mid;	
 	int pos;	
 
-	if(left>=right)
+	if(left>right)
 	{
 		return -1;
 	}	
 	
+	
+	begin = left;
+	end = right;
 	mid = (begin+end)/2;
+
+	if(nums[mid]==target)
+	{
+		pos = mid;
+		return pos;		
+	}
+	
 	if(nums[mid]<target)
 	{
 		begin = mid+1;
@@ -38,13 +48,15 @@ int search(int* nums, int numsSize, int target)
 	int right2;
 	
 	j=0;
-	
-	while(j<numsSize-2)
+	pos = numsSize-1;	
+	while(j<=numsSize-2)
 	{
 		if(nums[j+1]<nums[j])
 		{	
 			pos = j;
+		
 		}
+		j++;
 	}
 	
 	left1 = 0;
@@ -65,10 +77,11 @@ int search(int* nums, int numsSize, int target)
 
 void main()
 {
-	int nums[]={1,2,3,4,5};
+	int nums[]={1,2,3};
 	int target = 3;
-	int numsSize = 5;
+	int numsSize = 3;
 	int pos;
 	
 	pos = search(nums,numsSize,target);
+	printf("%d", pos);
 }
