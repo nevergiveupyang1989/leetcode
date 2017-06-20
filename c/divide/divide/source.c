831d8a4d2a8a6f963f09af70973de96826b1bde0
nevergiveupyang1989@gmail.com <nevergiveupyang1989@gmail.com>
2017-06-20 23:18:28 +0800

    利用位运算解决除法问题

--- c/divide/divide/source.c
+++ c/divide/divide/source.c
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int div(int x, int y)
+int newDiv(int a, int b)
 {
 	int isPos;
 	int digits;
-	digits = 0;
-	isPos = 0;
-	long res;
-
+	long long x,y,tmp,p;
+	isPos = 0;			//其值为1表示为正，0为负
+	int res;
+	
+	x = (long long)a;
+	y = (long long)b;
+	
 	if((x<0 && y<0) || (x>0 && y>0))
 	{
 		isPos = 1;
@@ -17,12 +20,21 @@ int div(int x, int y)
 	x = abs(x);
 	y = abs(y);
 
+	res = 0;	
 	while(x>=y)
 	{
-		y <<= 1;
-		digits++;
+		tmp = y;
+		p = 1;
+		while(x>(tmp<<1))
+		{
+			tmp <<= 1;
+			p <<= 1;	
+		}
+		res += p;
+		x -= tmp;
 	}
 	
+	return res;
 }
 
 int divide(int x, int y)
@@ -38,7 +50,7 @@ int divide(int x, int y)
 	
 	else
 	{
-		result = div(x,y);
+		result = newDiv(x,y);
 	}
 
 	return result;
@@ -48,7 +60,7 @@ void main()
 {
 	int result;
 	int x, y;
-	x=-2147483648;
+	x=0x80000000;
 	y=-1;
 	result = divide(x,y);
 
