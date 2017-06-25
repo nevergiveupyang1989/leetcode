c60b5cd48b18d00a96c854f491090eab251d978e
nevergiveupyang1989@gmail.com <nevergiveupyang1989@gmail.com>
2017-06-25 18:41:04 +0800

    优化找字典序的算法

--- c/nextPermutation/nextPermutation/source.c
+++ c/nextPermutation/nextPermutation/source.c
@@ -1,48 +1,74 @@
 #include <stdio.h>
 
+void reverseNums(int* nums, int start, int end)
+{
+	int i;
+	int tmp;
+	while(start<end)
+	{
+		tmp = nums[start];
+		nums[start] = nums[end];
+		nums[end] = tmp;
+		start++;
+		end--;
+	}
+}
+
 void nextPermutation(int* nums, int numsSize )
 {
 	int i,j;
 	int pos;
 	int tmp;	
+	int left;
+	int right;
+	int isAccend;	
+	int flag=0;               //此变量表示时时候找到可交换元素	
+	isAccend = 1;         //0表示不是降序，1表示降序
+	right = numsSize - 1;
+	left = numsSize - 2;
+	
+	while(left>=0)
+	{
+		while(right>left)
+		{
+			if(nums[right]>nums[left])
+			{
+				pos = left;
+				tmp = nums[right];
+				nums[right] = nums[left];
+				nums[left] = tmp;
+				isAccend = 0;
+				flag = 1;
+				break;
+			}
+			right--;
+		}
+		
+		if(flag==1)
+			break;         //元素已交换需要推出循环
+		left--;
+		right = numsSize;
+	}
+	
+	if(left==-1 && isAccend ==1)
+	{
+		reverseNums(nums, 0, numsSize-1);
+		return ;
+	}
 
-	for(i=numsSize; i-1>0; i--)
-	{
-		if(nums[i-1]>nums[i])
-		{
-			pos = i-1;
-			break;			
-		}
-	}
-	
-	for(i=numsSize; i>pos; i--)
-	{
-		if(nums[i]>nums[pos])
-		{
-			tmp = nums[i];
-			nums[i] = nums[pos];
-			nums[pos] = tmp;
-		}
-	}
-	
-	j = numsSize;
-	i = pos;
-	while(i<j)
-	{
-		tmp = nums[j];
-		nums[j] = nums[i];
-		nums[i] = tmp;
-		i++;
-		j--;
-	}	
+		
+	reverseNums(nums,pos+1,numsSize-1);
 }
 
 
 void main()
 {
-	int* nums = "12456";
-	int numsSize = 5;
+	int nums[] = {5,4,7,5,3,2};
+	int numsSize = 6;
+	int i;
 	nextPermutation(nums, numsSize);
-
-	printf("%s", nums);
+	
+	for(i=0; i<numsSize; i++){	
+		printf("%d", nums[i]);
+	}
 }
