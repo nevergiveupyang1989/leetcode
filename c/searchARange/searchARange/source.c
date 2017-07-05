dc3c067cdfb6211203b453453e939e2264143520
nevergiveupyang1989@gmail.com <nevergiveupyang1989@gmail.com>
2017-07-05 23:30:21 +0800

    考虑边界特殊情况

--- c/searchARange/searchARange/source.c
+++ c/searchARange/searchARange/source.c
@@ -1,90 +1,129 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int* searchRange(int* nums, int numsSize, int target, int* returnSize) 
 {
 	int left;
 	int right;
-	int res[] = {-1,-1};
+	int mid;	
+	int* res;	
+	
+	*returnSize = 2;
 
 	if(NULL==nums)
+	{
+		*returnSize = 0;
+		return res;
+	}
+
+	res = (int*)malloc(sizeof(int)*2);
+	if(NULL==res)
+		return NULL;
+	
+	res[0]=-1;
+	res[1]=-1;	
+
+	left = 0;
+	right = numsSize-1;
+
+	while(left<right-1)
+	{
+		mid = (right+left)/2;
+
+		if(target == nums[mid])
+		{
+			if(right==numsSize-1)
+				right = mid;           //寻找左半边
+			else
+				left = mid;
+		}
+		else if(target>nums[left])
+		{
+			left = mid;
+		}
+		else
+		{
+			right = mid;
+		}
+	}
+	
+	if(target == nums[left] || target == nums[right])
+	{
+		if (target == nums[left])
+			res[0] = left;
+		if (target == nums[right])
+			res[1] = right;
+	}
+	
+	else
+		return res;
+
+	if(res[1]!=-1 && res[0]!= -1)
+	{
+		return res;
+	}
+
+	left = 1;
+	right = numsSize-1;
+
+	while(left<right-1)
+	{
+		mid = (right+left)/2;
+		
+		if(target == nums[mid])
+		{
+			if(1==left)
+				left = mid;                //寻找右半边
+			else
+				right = mid;
+		}
+		else if(target>nums[mid])
+		{
+			right = mid;
+		}
+		else
+		{
+			left = mid;
+		}
+	}
+	
+	if(target == nums[left] || target == nums[right])
+	{
+		if(nums[left]==target)
+			res[0] = left;
+		if(nums[right]==target)
+			res[1] = right;
+	}
+	
+	if(res[0]==-1 && res[1]==-1)
 		return res;
 	
-	left = 0;
-	right = numsSize-1;
-
-	while(left<right-1)
+	if(res[0]==-1)
 	{
-		mid = left + (left+right)/2;
-		
-		if(target == nums[mid])
-		{
-			right = mid;           //寻找左半边
-		}
-		else if(target>nums[left])
-		{
-			left = mid;
-		}
-		else
-		{
-			right = mid;
-		}
+		res[0] = res[1];
 	}
+	else if(res[1]==-1)
+		res[1] = res[0];
 	
-	if(target == nums[left])
-	{
-		res[0] = left;
-	}
-	
-	else if(target == nums[right])
-	{
-		res[0] = right;
-	}
-	
-	else
-		return ret;
-
-}
-
-
-	left = 0;
-	right = numsSize-1;
-
-	while(left<right-1)
-	{
-		mid = left + (left+right)/2;
-		
-		if(target == nums[mid])
-		{
-			left = mid;                //寻找右半边
-		}
-		else if(target>nums[left])
-		{
-			left = mid;
-		}
-		else
-		{
-			right = mid;
-		}
-	}
-	
-	if(target == nums[left])
-	{
-		res[0] = left;
-	}
-	
-	else if(target == nums[right])
-	{
-		res[0] = right;
-	}
-	
-	else
-		return ret;
-	
-	return ret;
+	return res;
 }
 
 void main()
 {
+	int nums[] = {1,3};
+	int target = 1;
+	int* returnSize;
+	int numsSize = 2;
+	int i;	
+	int* res;	
 
+	returnSize = (int*)malloc(sizeof(int)*2);
+	res = searchRange(nums,numsSize,target,returnSize);
+
+	for(i=0; i<2; i++)
+	{
+		printf("%d", res[i]);
+	}
+	free(returnSize);
+	free(res);
 }
