0df21a1f600ebfdb16cdcc58011f9d5fe00bf4d5
nevergiveupyang1989@gmail.com <nevergiveupyang1989@gmail.com>
2017-07-23 21:09:04 +0800

    应用dfs，解决问题

--- c/combinationSum/combinationSum/source.c
+++ c/combinationSum/combinationSum/source.c
@@ -1,65 +1,87 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #define MAX 1000
 
-int top;
-int rows = 0;      //stack top
-int columnMax = 0;
+int rows = -1;      //
+int column = -1;
 
-void allAnswerSstackPush(int** nums, int* oneAnswers)
+void allAnswerSstackPush(int** nums, int* oneAnswers, int* returnSize, int** columnsSize)
 {
-	nums[rows++] = oneAnswers;
+	int i;
+	rows++;
+	nums[rows] = (int*)malloc(sizeof(column));
 	
-	if(top>columnMax)
-	{
-		columnMax = top;
+	for(i=0; i<=column; i++)
+	{	
+		nums[rows][i] = oneAnswers[i];
 	}
-
-	top=0;
+	*returnSize = rows+1;
+	columnsSize[rows][0] = column+1;
 }
 
-void oneAnswersStackPop()
+void oneAnswersStackPop(int* oneAnswers)
 {
-	top--;
+	oneAnswers[column]=-1;
+	column--;
 }
 
 void oneAnswersStackPush(int* oneAnswers, int num)
 {
-	oneAnswers[top++] = num;
+	oneAnswers[++column] = num;
 }
 
-void dfs(int** nums, int target, const int* candidates, int candidatesSize, int* oneAnswers)
+int getTargetPos(int* candidates, int target, int candidatesSize)
+{
+	int i;
+	int pos;
+
+	pos = -1;
+	for(i=0; i<candidatesSize; i++)
+	{
+		if(target==candidates[i])
+			pos = i;
+	}
+	
+	return pos;
+}
+
+void dfs(int** nums, int target,int* candidates,int candidatesSize, int* oneAnswers, int* returnSize, int** columnsSize)
 {
 	int i;
 	int size;	
-	
+	int pos;	
+
 	size = candidatesSize;
 	
 	if(target==0)
 	{
-		stackPush(nums, oneAnswers);
+		allAnswerSstackPush(nums, oneAnswers, returnSize,columnsSize);
 	}
 	
 	for(i=0; i<size; i++)
 	{
 		if(target<candidates[i])
 			return;
-		oneAnswersStackPush(oneAnswers, candidates);	
-		dfs(nums, target-candidates[i],candidates, candidatesSize, oneAnswers);
-		stackPush(candidates[i]);
-		oneAnswersStackPop();
+		pos = getTargetPos(candidates, oneAnswers[column+1], candidatesSize);
+		
+		if(i>=pos || -1==pos)		
+			oneAnswersStackPush(oneAnswers, candidates[i]);	
+		dfs(nums, target-candidates[i],candidates, candidatesSize, oneAnswers, returnSize, columnsSize);
+		//stackPush(candidates[i]);
+		oneAnswersStackPop(oneAnswers);
 	}
 }
 
-void sort(int* condidates, int condidates)
+void sort(int* condidates, int condidatesSize)
 {
 	int i;
 	int j;
 	int temp;	
 
-	for(i=0; i<condidates; i++)
+	for(i=0; i<condidatesSize; i++)
 	{
-		for(j=i; j<condidates; j++)
+		for(j=i; j<condidatesSize; j++)
 		{
 			if(condidates[i]>condidates[j])
 			{
@@ -71,27 +93,60 @@ void sort(int* condidates, int condidates)
 	}
 }
 
-int** combinationSum(int* candidates, int candidatesSize, int target, int** columnSizes, int* returnSize)
+int** combinationSum(int* candidates, int candidatesSize, int target, int** columnsSize, int* returnSize)
 {
 	int** nums;
 	int* oneAnswers;
 
-	nums = (int**)malloc(sizof(int*)*MAX);
+	nums = (int**)malloc(sizeof(int*)*MAX);
+	if(NULL==nums)
+		return NULL;
+		
+	oneAnswers = (int*)malloc(sizeof(int)*MAX);
 	if(NULL==nums)
 		return NULL;
 	
-	oneAnswers = (int**)malloc(sizof(int*)*MAX);
-	if(NULL==nums)
-		return NULL;
+	memset(oneAnswers, -1, MAX*sizeof(int));
+	dfs(nums, target, candidates, candidatesSize, oneAnswers, returnSize, columnsSize);
 	
-	dfs(nums, target, condidates, condidatesSize, oneAnswers);
-	
-	*returnSize = rows + 1;
-	**columnSize = columnMax;	
 	return nums;
 }
 
 void main()
 {
-	int a[][3]={{1},{1,2,3}};
+	int a[4]={2,3,6,7};
+	int size;
+	int target;	
+	int** columnsSize;
+	int* returnSize;
+	int** result;	
+	int i,j;
+	size = 4;
+	target = 7;
+	
+	returnSize = (int*)malloc(sizeof(int)*1);
+	columnsSize = (int**)malloc(sizeof(int*)*MAX);
+	
+	for(i=0; i<MAX; i++)
+	{
+		columnsSize[i] = (int*)malloc(sizeof(int)*1);
+	}
+	
+	result = combinationSum(a, size, target, columnsSize, returnSize);	
+	
+	for(i=0; i<*returnSize; i++)
+	{
+		for(j=0; j<(*columnsSize[i]); j++)
+		{
+			printf("%d\n", result[i][j]);
+		}
+	}
+	
+/*	for(i=0; i<(*returnSize); i++)
+	{
+		free(result[i]);
+	}
+	
+	free(result);
+*/
 }
