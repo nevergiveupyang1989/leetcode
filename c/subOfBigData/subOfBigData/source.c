b54dae37da8d5bbd13c3c705502e0f3b141127ad
nevergiveupyang1989@gmail.com <nevergiveupyang1989@gmail.com>
2017-08-11 23:07:13 +0800

    修改大整数存储方式，并修改算法

--- c/subOfBigData/subOfBigData/source.c
+++ c/subOfBigData/subOfBigData/source.c
@@ -2,10 +2,9 @@
 #include <stdio.h>
 #include <string.h>
 #define MAX 200
-#define MAX 200
 
-char ans1[MAX];
-char ans2[MAX];
+char ans1[MAX]={0};
+char ans2[MAX]={0};
 
 int subOfBigData(char* nums1, char* nums2)
 {
@@ -14,19 +13,22 @@ int subOfBigData(char* nums1, char* nums2)
 	int isPositive;	
 	int i,j;	
 	int maxSize;	
-	int sumSize;	
+	int subSize;	
 	char* temp;	
 
 	isPositive= 1;
-	size1 = strlen(nums1);
-	size2 = strlen(nums2);
+	size1 = 0;
+	size2 = 0;
 	
-	memset(ans1, 0, MAX);
-	memset(ans2, 0, MAX);
+	while(nums1[size1]!='\0')
+		size1++;
 	
+	while(nums2[size2]!='\0')
+		size2++;
+
 	maxSize = (size1>size2)?size1:size2;
 
-	if(size1<size2)
+	if(size1<size2 || (size1==size2) && nums1[size1-1]< nums2[size2-1] )
 	{
 		temp = nums2;
 		nums2 = nums1;
@@ -37,46 +39,61 @@ int subOfBigData(char* nums1, char* nums2)
 	j=0;
 	for(i=size1-1; i>=0; i--)
 	{
-		ans1[j++] = nums1[i];
+		ans1[j++] = nums1[i]-'0';
 	}
 
 	j=0;
 	for(i=size2-1; i>=0; i--)
 	{
-		ans2[j++] = nums2[i];
+		ans2[j++] = nums2[i]-'0';
 	}
 
 	for(i=0; i<MAX; i++)
 	{
-		ans1[i] = (ans1[i]-'0') - (ans2[i]-'0');
+		ans1[i] = ans1[i] - ans2[i];
 		
 		if(ans1[i]<0)
 		{
-			ans1[i] += 10;
+			ans1[i] = 0 - ans1[i];
 			ans1[i+1]--;
 		}
+		ans1[i] += '0';
 	}
 	
-	sumSize = (ans1[maxSize-1]!=0)?(maxSize-1):(maxSize-2);
+	subSize = (ans1[maxSize-1]!=0)?maxSize:(maxSize-1);
 	
 	if(isPositive==0)
 	{
-		ans1[i] = '-';
+		ans1[subSize] = '-';
+		subSize++;
 	}
 
-	return sumSize;
+	return subSize;
 }
 
 void main()
 {
-	char* nums1;
-	char* nums2;
+	char nums1[MAX]={0};
+	char nums2[MAX]={0};
 	int size;
+	int i;
+	int isPrint;	
 
 	scanf("%s", nums1);
 	scanf("%s", nums2);
 
 	size = subOfBigData(nums1, nums2);
 	
-	printf("%d", size);
+	isPrint = 0;
+	for(i=MAX; i>=0; i--)
+	{
+		if (isPrint!=0)
+			printf("%c", ans1[i]);	
+		
+		if (ans1[i]!= '0' && isPrint==0)
+		{
+			printf("%c", ans1[i]);
+			isPrint = 1;	
+		}
+	}
 }
