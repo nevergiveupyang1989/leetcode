be8dac1f4357a93a58959bc9531f6c9146baba6f
nevergiveupyang1989@gmail.com <nevergiveupyang1989@gmail.com>
2017-08-30 08:45:25 +0800

    增加对指数为最小负数的考虑

--- c/pow/pow/source.c
+++ c/pow/pow/source.c
@@ -1,17 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MIN 0x80000000
 
 double myPow(double x, int n)
 {
 	double result;
 
-	if(0==n)
+	if(0 == x)
+	{
+		return 0;
+	}
+
+	if(0==n || (x == -1 && n%2 == 0) || x== 1)
 	{
 		return 1;
 	}
 	
 	if(n<0)
 	{
+		if(n == MIN)
+		{
+			n += 1;
+		}
+
 		n = 0-n;
 		x = 1/x;
 	}
@@ -26,7 +37,7 @@ void main()
 	int n;
 	
 	x = 3;
-	n = -2;
+	n = -2147483648;
 
 	result = myPow(x, n);
 	
