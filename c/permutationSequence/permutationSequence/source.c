fb96bfea7d07480a00079ddb41cb35a7f36746b6
nevergiveupyang1989@gmail.com <nevergiveupyang1989@gmail.com>
2017-10-15 17:22:31 +0800

    输出全排列第k个序列

--- c/permutationSequence/permutationSequence/source.c
+++ c/permutationSequence/permutationSequence/source.c
@@ -1,36 +1,77 @@
 #include <stdio.h>
 #include <string.h>
+#include <malloc.h>
 char* getNums(int n)
 {
-	return NULL;
+	char* nums = NULL;
+	int i;
+
+	if(0 == n)
+		return NULL;
+	
+	nums = (char*)malloc(sizeof(char)*n);
+	if(NULL==nums)
+		return NULL;	
+
+	for(i=0; i<n; i++)
+	{
+		nums[i] = i+1;
+	}
+	
+	return nums;	
 }
 
 
-void swap(int* nums, int a, int b)
+void swap(char* nums, int index, int start)
 {
 	int temp;
-	temp = nums[a];
-	nums[a] = nums[b];
-	nums[b] = temp;
+	int i;
+
+	temp = nums[index];
+	
+	i = index - 1;
+	while(i>=start)
+	{
+		nums[i+1] = nums[i];
+		i--;
+	}	
+	
+	nums[start] = temp;
 }
 
-void permutationSeq(int* nums, int start, int end)
+char* permutationSeq(char* nums, int start, int end, int k, int* cnt)
 {
 	int i;
+	char* result;	
+	
+	result = NULL;
 
 	if(start == end)
 	{
-		for(i=0; i<end; i++)
+		if(*cnt==k-1)
+		{
+			result = (char*)malloc(sizeof(char)*end);
+			for(i=0; i<end; i++)
+			{
+				result[i] = nums[i];
+			}
+		}
+		
+		*cnt++;	
+		return result;
+		/*for(i=0; i<end; i++)
 		{
 			printf("%d", nums[i]);
 		}
-		printf("\n");
+		printf("\n");*/
 	}
 	
 	for(i=start; i<end; i++)
 	{
 		swap(nums, i, start);
-		permutationSeq(nums, start+1, end);
+		result = permutationSeq(nums, start+1, end, k, cnt);
+		if(NULL!=result)
+			return result;
 		swap(nums, i, start);
 	}
 }
@@ -38,21 +79,33 @@ void permutationSeq(int* nums, int start, int end)
 char* getPermutation(int n, int k)
 {
 	int start,end;
-
-	int nums[] = {1,2,3};
-	start = 0;
-	end = 3;
-
-	permutationSeq(nums, start, end);
+	char* nums;
+	int* cnt;
+	char* result;
+	//int nums[] = {1,2,3};
 	
-	return NULL;
+	cnt = (int*)malloc(sizeof(int)*1);
+	*cnt = 0;
+
+	nums = getNums(n);
+	start = 0;
+	end = n;
+	result = permutationSeq(nums, start, end, k, cnt);
+	
+	return result;
 }
 
 void main()
 {
 	int n,k;
 	n = 3;
-	k = 3;
-	
-	getPermutation(n,k);
+	k = 6;
+	int i;
+	char* result;
+
+	result = getPermutation(n,k);
+	for(i=0; i<n; i++)
+	{
+		printf("%d", result[i]);
+	}
 }
