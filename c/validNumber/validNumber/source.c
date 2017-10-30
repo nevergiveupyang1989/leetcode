d9fc43f9c5e22165a614a58b195e7005731db235
nevergiveupyang1989@gmail.com <nevergiveupyang1989@gmail.com>
2017-10-30 22:56:03 +0800

    判断一个字符串时候是数字（除指数外）

---
+++ c/validNumber/validNumber/source.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define bool int
+#define True 1
+#define False 0
+
+bool isNumber(char* s)
+{
+	int i;
+	int signNum;
+	int pointNum;	
+	int strSize;
+	int digitNum;
+		
+	strSize = strlen(s);	
+
+	while(s[i]==' ')
+		i++;
+	
+	signNum = 0;
+	while(s[i]=='+' || s[i]=='-')
+	{
+		signNum++;
+		i++;	
+	}
+	
+	if(signNum>1)
+		return False;
+	
+	pointNum = 0;
+	digitNum = 0;
+	for(; (s[i]=='.' || (s[i]>='1' && s[i]<='9'))&&i<strSize ;i++)
+	{
+		if(s[i]=='.')
+			pointNum++;
+		else
+			digitNum++;
+	}
+	
+	if(pointNum>1 || digitNum<1)
+		return False;
+	else
+		return True;	
+
+	return s[i]=='0';
+}
+
+
+void main()
+{
+	bool result;
+	char* s = "123";
+	result = isNumber(s);
+
+	printf("%d",result);
+}
