f88ce8703ddbfc7a8a3a5663eef7da3e00420bcc
nevergiveupyang1989@gmail.com <nevergiveupyang1989@gmail.com>
2017-11-14 23:33:18 +0800

    修改算法，增加一维标记该行该列是否有0元素

--- c/setZeroes/setZeroes/source.c
+++ c/setZeroes/setZeroes/source.c
@@ -1,110 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-void setZeroes(int(* matrix)[3], int matrixRowSize, int matrixColSize) {
+void setZeroes(int(* matrix)[9], int matrixRowSize, int matrixColSize) {
 	int i,j;
-	int isClear;
+	int* addRows;
+	int* addCols;
+
+	addRows = (int*)malloc(sizeof(int)*matrixRowSize);     //在原来矩阵的基础上，增加一维记录所在行/列有无0元素
+	addCols = (int*)malloc(sizeof(int)*matrixColSize);
 	
-	isClear = 0;
+	if(NULL==addRows || NULL==addCols)
+		return;
+
+	memset(addRows, 0, sizeof(int)*matrixRowSize);      //执行初始化，1表示该行/列有0元素，0表示没有
+	memset(addCols, 0, sizeof(int)*matrixColSize);
+	
 	if(NULL==matrix)
 	    return;
 	
-	if(matrixRowSize==1 || matrixColSize==1)
+	for(i=0; i<matrixRowSize; i++)
 	{
-		if(matrixRowSize==1)
-		{
-			for(i=0; i<matrixColSize;i++)
-			{
-				if(matrix[0][i]==0)
-				{
-					isClear = 1;
-					break;
-				}
-			}
-			
-			if(isClear == 0)
-				return;
-
-			for(i=0; i<matrixColSize; i++)
-			{
-				matrix[0][i] = 0;
-			}
-		}
-		else
-		{
-			for(i=0; i<matrixRowSize; i++)
-			{
-				if(matrix[i][0]==0)
-				{
-					isClear = 1;
-					break;
-				}
-			}
-			
-			if(isClear==0)
-				return;
-
-			for(i=0; i<matrixRowSize; i++)
-			{
-				matrix[i][0] = 0;
-			}
-		}
-
-		return;
-	}
-
-	else{
-		for(i=1; i<matrixRowSize; i++)
-		{
-			for(j=1; j<matrixColSize; j++)
-			{
-				if(matrix[i][j]==0)
-				{
-					matrix[i][0] = 0;
-					matrix[0][j] = 0;
-				}	
-			}
-		}
-
-		for(i=0; i<matrixRowSize; i++)
-		{
-			if(matrix[i][0]==0)
-			{
-				for(j=0; j<matrixColSize; j++)
-				{
-					matrix[i][j] = 0;
-				}
-			}
-		}
-
-
 		for(j=0; j<matrixColSize; j++)
 		{
-			if(matrix[0][j]==0)
+			if(matrix[i][j]==0)
 			{
-				for(i=0; i<matrixRowSize; i++)
-				{
-					matrix[i][j] = 0;
-				}
+				addRows[j] = 1;
+				addCols[i] = 1;
+			}	
+		}
+	}
+	
+	for(i=0; i<matrixRowSize; i++)
+	{
+		if(addRows[i]==1)
+		{
+			for(j=0; j<matrixColSize; j++)
+			{
+				matrix[i][j] = 0;
 			}
 		}
 	}
+
+	for(j=0; j<matrixColSize; j++)
+	{
+		for(i=0; i<matrixRowSize; i++)
+		{
+			if(addRows[i]==0 && addCols[j]==0){
+				continue;
+			}
+			else
+				matrix[i][j] = 0;
+		}	
+	}
+
+	free(addRows);
+	free(addCols);
 	return;
 }
 
 void main()
 {
-	int matrix[][3] = {{1,2,3},{1,0,3}, {1,3,0}};
+	int matrix[][9] = {{8,3,1,4,6,4,0,3,4},{9,1,3,0,1,5,7,4,1},{2,2,5,7,5,4,4,3,8},{4,9,7,0,3,6,9,5,9},{4,1,8,8,4,1,5,7,6}};
 	int rows,cols;
 	int i,j;
 
-	rows = 3;
-	cols = 3;
+	rows = 5;
+	cols = 9;
 	setZeroes(matrix, rows, cols);
 	
-	for(i=0; i<3; i++)
+	for(i=0; i<rows; i++)
 	{
-		for(j=0; j<3; j++)
+		for(j=0; j<cols; j++)
 		{
 			printf("%d", matrix[i][j]);
 		}
