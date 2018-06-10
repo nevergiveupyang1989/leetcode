464a2ffdf207f0168384508e1667f94da9e97845
“yang” <lovepython@foxmail.com>
2018-06-10 17:19:25 +0800

    增加单元测试

--- py/pathsum2.py
+++ py/pathsum2.py
@@ -1,5 +1,6 @@
 #! usr/local/bin/python
-
+import unittest
+import copy 
 
 class TreeNode:
     def __init__(self,val):
@@ -17,29 +18,61 @@ class Solution:
         if root is None:
             return 0
         if not root.left and not root.right:
-            if root.val == sum:
+            if root.val == num:
                 self.tmp.append(root.val)
-                self.res.append(self.tmp)
+                self.res.append(copy.copy(self.tmp))
+
+                return True 
             else:
                 self.tmp = []
+                return False 
+        else: 
+            self.tmp.append(root.val)
         
-        self.tmp.append(root.val)
+        left = False 
+        right = False 
 
-        if not root.left:
-            self.pathSum(root.left, sum-root.val)
+        if root.left:
+            left = self.pathSum(root.left, num-root.val)
+            
+            if left:
+                self.tmp.pop()
 
-        if not root.right:
-            self.pathSum(root.right, sum-root.val)
+        if root.right:
+            right = self.pathSum(root.right, num-root.val)
+            
+        return left or right 
 
-        return self.res 
+class TestpathSum(unittest.TestCase):
+    def setUp(self):
+        self.solution = Solution()
+        self.testfunc = self.solution.pathSum  
+        
+    def tearDown(self):
+        del self.testfunc 
+
+    def testNoneNode(self):
+        curNode = None
+        num = 1
+        self.testfunc(curNode, num)
+        res = self.solution.res 
+        self.assertListEqual(res,[])
+
+    def testExistOnePath(self):
+        curNode = TreeNode(0)
+        curNode.left = TreeNode(1)
+        curNode.right = TreeNode(2)
+        self.testfunc(curNode, 1)
+        res = self.solution.res 
+        self.assertListEqual(res,[[0,1]])
+
+    def testExistMulPath(self):
+        curNode = TreeNode(0)
+        curNode.left = TreeNode(1)
+        curNode.right = TreeNode(1)
+        self.testfunc(curNode, 1)
+        res = self.solution.res 
+        self.assertListEqual(res, [[0,1],[0,1]])
 
 if __name__ == '__main__':
-    curNode = TreeNode(0)
-    curNode.left = TreeNode(1)
-    curNode.right  = TreeNode(2)
-
-    res = Solution().pathSum(curNode, 1)
-    
-    print res
-
-        
+    unittest.main()
