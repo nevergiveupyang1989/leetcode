0e1e0161b8280d9bbfec0fde1c5868b6c7a77c80
“yang” <lovepython@foxmail.com>
2018-07-29 13:24:59 +0800

    修改为0(n)的算法

--- py/longestSquence.py
+++ py/longestSquence.py
@@ -1,6 +1,7 @@
 # usr/local/bin/python
 
 class Solution(object):
+    '''
     def longestConsecutive(self,nums):
         if nums is None:
             return 0
@@ -17,7 +18,41 @@ class Solution(object):
                 length = 1
 
         return maxLength 
+    '''
 
+    def longestConsecutive(self,num):
+        numsDict = {}
+        maxLength = 0
+        tmp = 0
+
+        if num is None:
+            return maxLength 
+
+        for i in num:
+            if i not in numsDict.keys(): 
+                preidx = i
+                latteridx = i
+                numsDict[i] = 1 
+                
+                while preidx in numsDict.keys():
+                    preidx -= 1
+                
+                if preidx+1 != i:
+                    numsDict[i] += numsDict[preidx+1]
+
+                while latteridx in numsDict.keys():
+                    latteridx += 1
+                
+                if latteridx-1 != i:
+                    numsDict[latteridx-1] += numsDict[i]
+                
+                numsDict[preidx+1] = numsDict[latteridx-1]
+                 
+        for i in numsDict.values():
+            if i > maxLength:
+                maxLength = i
+        
+        return maxLength
 
 if __name__ == '__main__':
     s = Solution()
