6c078fd5f36be6da6821a54c28bebb2782b6c903
“yang” <lovepython@foxmail.com>
2018-08-08 08:56:21 +0800

    判断字符串的子串是回文字符串

--- py/splitPalindrome.py
+++ py/splitPalindrome.py
@@ -7,21 +7,30 @@ class Solution(object):
             return res 
 
         length = len(s)
-        for i in range(1,length):
+        for i in range(1,length+1):
             if self.isPalindrome([s[:i]]):
                 res.append([s[:i]])
+            if self.isPalindrome(s[i:]):
+                res.append(s[i:])
         
         return res 
 
     def isPalindrome(self, s):
+        if s == []:
+            return False 
+
         length = len(s)
         start = 0
         end = length - 1
 
         while start != end:
-            if s[start] != s[end]:
+            if start < end and s[start] != s[end]:
+                start += 1
+                end -= 1
                 return False 
             else:
+                start += 1
+                end -= 1
                 continue 
 
         return True 
