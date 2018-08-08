83e9093b62b1d4da303e0dae26aac6eab1d71ab5
“yang” <lovepython@foxmail.com>
2018-08-08 08:39:30 +0800

    修改求输入行数和列数

--- py/surroundedRegions.py
+++ py/surroundedRegions.py
@@ -5,15 +5,14 @@ class Solution(object):
         if board == []:
             return
 
-        rowSize = len(board[0])
-        colSize = len(board)
+        rowSize = len(board)
+        colSize = len(board[0])
 
         for i in xrange(rowSize):
             for j in xrange(colSize):
                 if i == 0 or i == rowSize-1 or j == 0 or j == colSize-1:
                     if board[i][j] == 'O':
                         self.dfs(board, i, j)
-        print board
         for i in xrange(rowSize):
             for j in xrange(colSize):
                 if board[i][j] == 'O':
@@ -29,7 +28,7 @@ class Solution(object):
         board[rowIdx][colIdx] = '*'
 
         for i in xrange(4):
-            if rowIdx + row[i]>= 0 and rowIdx + row[i] < len(board[0]) and colIdx + col[i] >= 0 and colIdx+col[i] < len(board) and board[rowIdx+row[i]][colIdx+col[i]] == 'O':
+            if rowIdx + row[i]>= 0 and rowIdx + row[i] < len(board) and colIdx + col[i] >= 0 and colIdx+col[i] < len(board[0]) and board[rowIdx+row[i]][colIdx+col[i]] == 'O':
                 self.dfs(board, rowIdx+row[i], colIdx+col[i])
 
 
